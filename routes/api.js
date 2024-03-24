4ba0872c84e6dd3fe3bb6505bfde93731dfe5cb0
VanzDev <152618282+ManusiaTamvanzzz@users.noreply.github.com>
2024-03-25 00:05:22 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -63,7 +63,7 @@ loghandler = {
 	if (!url) return res.json(loghandler.noturl)   
 	ssweb(url, 'desktop').then((data) =>{ 
 	res.set({'Content-Type': 'image/png'})
-        let buffer = await fetch(data.data)
+        let buffer = await fetch(data)
 	res.send(await buffer.buffer())
 	})
         .catch((err) =>{
